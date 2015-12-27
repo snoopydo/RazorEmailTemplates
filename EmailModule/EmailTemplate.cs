68f283aed941de9695ed12d86d635d93e5bb2fe6
David Smith <db.ka.smith@gmail.com>
2015-12-27 23:00:47 +1300

    update to use current System.Web.Razor
    Also implements Html & Text razor sections for email content.

--- EmailModule/EmailTemplate.cs
+++ EmailModule/EmailTemplate.cs
@@ -1,64 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Web.Razor;
+
 namespace EmailModule
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Text;
+	public delegate void SectionWriter();
 
-    public abstract class EmailTemplate : IEmailTemplate
-    {
-        private readonly StringBuilder buffer;
+	public abstract class EmailTemplate : IEmailTemplate
+	{
+		private readonly StringBuilder buffer;
+		private Dictionary<string, SectionWriter> SectionWriters = new Dictionary<string, SectionWriter>();
 
-        [DebuggerStepThrough]
-        protected EmailTemplate()
-        {
-            To = new List<string>();
-            ReplyTo = new List<string>();
-            CC = new List<string>();
-            Bcc = new List<string>();
-            Headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+		[DebuggerStepThrough]
+		protected EmailTemplate()
+		{
+			To = new List<string>();
+			ReplyTo = new List<string>();
+			CC = new List<string>();
+			Bcc = new List<string>();
+			Headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
-            buffer = new StringBuilder();
-        }
+			buffer = new StringBuilder();
+		}
 
-        public string From { get; set; }
+		public string From { get; set; }
 
-        public string Sender { get; set; }
+		public string Sender { get; set; }
 
-        public ICollection<string> To { get; private set; }
+		public ICollection<string> To { get; private set; }
 
-        public ICollection<string> ReplyTo { get; private set; }
+		public ICollection<string> ReplyTo { get; private set; }
 
-        public ICollection<string> CC { get; private set; }
+		public ICollection<string> CC { get; private set; }
 
-        public ICollection<string> Bcc { get; private set; }
+		public ICollection<string> Bcc { get; private set; }
 
-        public IDictionary<string, string> Headers { get; private set; }
+		public IDictionary<string, string> Headers { get; private set; }
 
-        public string Subject { get; set; }
+		public string Subject { get; set; }
 
-        public string Body
-        {
-            get { return buffer.ToString(); }
-        }
+		public string Body {get;private set;}
+		public string Html { get; private set; }
+		public string Text { get; private set; }
+		
+		protected dynamic Model { get; private set; }
 
-        protected dynamic Model { get; private set; }
+		public void SetModel(dynamic model)
+		{
+			Model = model;
+		}
 
-        public void SetModel(dynamic model)
-        {
-            Model = model;
-        }
+		// Razor will override and implement this method
+		public abstract void Execute();
 
-        public abstract void Execute();
+		public void Render()
+		{
+			Execute();
+			Body = buffer.ToString();
 
-        public virtual void Write(object value)
-        {
-            WriteLiteral(value);
-        }
+			if (SectionWriters.ContainsKey("Html"))
+			{
+				buffer.Clear();
+				SectionWriters["Html"].Invoke();
+				Html = buffer.ToString();
+			}
 
-        public virtual void WriteLiteral(object value)
-        {
-            buffer.Append(value);
-        }
-    }
+			if (SectionWriters.ContainsKey("Text"))
+			{
+				buffer.Clear();
+				SectionWriters["Text"].Invoke();
+				Text = buffer.ToString();
+			}
+
+
+		}
+
+		public virtual void Write(object value)
+		{
+			WriteLiteral(value);
+		}
+
+		public virtual void WriteLiteral(object value)
+		{
+			buffer.Append(value);
+		}
+
+		public virtual void WriteAttribute(string name, PositionTagged<string> prefix, PositionTagged<string> suffix, params AttributeValue[] values)
+		{
+			bool first = true;
+			bool wroteSomething = false;
+			if (values.Length == 0)
+			{
+				// Explicitly empty attribute, so write the prefix and suffix 
+				WritePositionTaggedLiteral(prefix);
+				WritePositionTaggedLiteral(suffix);
+			}
+			else
+			{
+				for (int i = 0; i < values.Length; i++)
+				{
+					AttributeValue attrVal = values[i];
+					PositionTagged<object> val = attrVal.Value;
+					PositionTagged<string> next = i == values.Length - 1 ?
+						suffix : // End of the list, grab the suffix 
+						values[i + 1].Prefix; // Still in the list, grab the next prefix 
+
+
+					bool? boolVal = null;
+					if (val.Value is bool)
+					{
+						boolVal = (bool)val.Value;
+					}
+
+
+					if (val.Value != null && (boolVal == null || boolVal.Value))
+					{
+						string valStr = val.Value as string;
+						if (valStr == null)
+						{
+							valStr = val.Value.ToString();
+						}
+						if (boolVal != null)
+						{
+							Debug.Assert(boolVal.Value);
+							valStr = name;
+						}
+
+
+						if (first)
+						{
+							WritePositionTaggedLiteral(prefix);
+							first = false;
+						}
+						else
+						{
+							WritePositionTaggedLiteral(attrVal.Prefix);
+						}
+
+
+						// Calculate length of the source span by the position of the next value (or suffix) 
+						int sourceLength = next.Position - attrVal.Value.Position;
+
+
+						if (attrVal.Literal)
+						{
+							WriteLiteral(valStr);
+						}
+						else
+						{
+							Write(valStr); // Write value 
+						}
+						wroteSomething = true;
+					}
+				}
+				if (wroteSomething)
+					WritePositionTaggedLiteral(suffix);
+			}
+		}
+
+		public void DefineSection(string name, SectionWriter action)
+		{
+			if (SectionWriters.ContainsKey(name))
+			{
+				throw new ApplicationException(string.Format("Section {0}, already defined.",name)); 
+			}
+			SectionWriters[name] = action;
+		}
+
+		private void WritePositionTaggedLiteral(string value, int position)
+		{
+			if (value == null)
+				return;
+
+
+			WriteLiteral(value);
+		}
+
+		private void WritePositionTaggedLiteral(PositionTagged<string> value)
+		{
+			WritePositionTaggedLiteral(value.Value, value.Position);
+		}
+
+		
+	}
 }
--- EmailModule/IEmailTemplate.cs
+++ EmailModule/IEmailTemplate.cs
@@ -1,29 +1,29 @@
 namespace EmailModule
 {
-    using System.Collections.Generic;
+	using System.Collections.Generic;
 
-    public interface IEmailTemplate
-    {
-        string From { get; set; }
+	public interface IEmailTemplate
+	{
+		string From { get; set; }
 
-        string Sender { get; set; }
+		string Sender { get; set; }
 
-        ICollection<string> To { get; }
+		ICollection<string> To { get; }
 
-        ICollection<string> ReplyTo { get; }
+		ICollection<string> ReplyTo { get; }
 
-        ICollection<string> CC { get; }
+		ICollection<string> CC { get; }
 
-        ICollection<string> Bcc { get; }
+		ICollection<string> Bcc { get; }
 
-        IDictionary<string, string> Headers { get; }
+		IDictionary<string, string> Headers { get; }
 
-        string Subject { get; set; }
+		string Subject { get; set; }
 
-        string Body { get; }
+		string Body { get; }
 
-        void SetModel(dynamic model);
+		void SetModel(dynamic model);
 
-        void Execute();
-    }
+		void Render();
+	}
 }
